551d550cb194d2625c855432fe2ec7644d37dd4c
Dhaniesh Murugan <dhaniesh.m@idp.com>
2025-09-03 09:06:21 +0530

    Add employee list and detail API endpoints

--- backend/main.py
+++ backend/main.py
@@ -7,16 +7,26 @@ from models import Employees
 
 app = FastAPI()
 
-@app.get("/health")
+@app.get("/employee")
 def health(db: Session = Depends(get_db)) -> List[dict]:
     employees = db.query(Employees).all()
     employee_list = []
     for employee in employees:
         employee_list.append({
             "id": employee.id,
-            "uuid": str(employee.uuid),
             "firstName": employee.firstName,
             "lastName": employee.lastName,
             "email": employee.email
         })
     return JSONResponse(employee_list, status.HTTP_200_OK)
+
+@app.get("/employee/{id}")
+def health(id, db: Session = Depends(get_db)) -> List[dict]:
+    employee = db.query(Employees).filter(Employees.id == id).first()
+    employee = {
+        "id": employee.id,
+        "firstName": employee.firstName,
+        "lastName": employee.lastName,
+        "email": employee.email
+    }
+    return JSONResponse(employee, status.HTTP_200_OK)
