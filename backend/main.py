4af38453613519f3f44ac83eb5fe26edd2c39513
Dhaniesh Murugan <dhaniesh.m@idp.com>
2025-09-10 00:07:08 +0530

    backend: add cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from routers import employee, room, meeting
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = ["http://localhost:3000", "http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=employee.router)
 app.include_router(router=room.router)
