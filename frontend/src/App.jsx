2563cc1446870f1915db4ed8df9bf7ce8c8fba7f
Dhaniesh Murugan <dhaniesh.m@idp.com>
2025-09-09 23:31:39 +0530

    frontend: base template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
   return (
     <>
-      <div>
-        <p className='text-6xl font-bold flex justify-center'>Room Sync</p>
+      <div className="font-serif flex flex-col justify-center items-center gap-y-4 text-center pb-40">
+        <p className="text-6xl font-bold">Room Sync</p>
+        <p className="text-xl">Book a meeting now</p>
+      </div>
+      <div className="font-serif flex justify-center gap-x-20">
+        <div>
+          <p className="text-xl">Start time</p>
+        </div>
+        <p className="text-xl">End time</p>
+        <p className="text-xl">Available rooms</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
