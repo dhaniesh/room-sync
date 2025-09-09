cc6b81b3514e85cb431030d76989a3bc602485b5
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-09-10 00:29:26 +0530

    frontend: refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     async function fetchRooms() {
       try {
         const response = await axios.get("http://localhost:8000/room");
-        
+
         setRooms(response.data);
       } catch (error) {
         console.error("Error fetching rooms:", error);
