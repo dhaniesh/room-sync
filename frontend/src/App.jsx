2fb0bbe17d3316de0b8ab4fc7f6cc41720764afc
Dhaniesh Murugan <dhaniesh.m@idp.com>
2025-09-09 23:00:23 +0530

    frontend init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='text-6xl font-bold flex justify-center'>Room Sync</p>
+      </div>
+    </>
+  )
+}
+
+export default App
